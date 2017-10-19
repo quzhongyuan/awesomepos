6b22e087432878068f8fa6341427244f8058b512
quzhongyuan <878773231@qq.com>
2017-10-19 09:08:28 +0800

    vue-cli简单demo

---
+++ src/App.vue
@@ -0,0 +1,88 @@
+
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+31
+32
+33
+34
+35
+36
+37
+38
+39
+40
+41
+42
+<template>
+  <div id="app">
+    <!--左侧导航-->
+    
+        <leftNav></leftNav>
+    
+    <!--操作区域-->
+    <div class="main">
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
+ 
+<script>
+import leftNav from '@/components/common/leftNav'
+export default {
+  name: 'app',
+  components:{
+   leftNav
+  }
+}
+</script>
+ 
+<style>
+body{
+  margin: 0px;
+  padding: 0px;
+}
+#app {
+  font-family: 'Microsoft YaHei','Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: left;
+  color: #2c3e50;
+   height:100%;
+}
+ 
+.main{
+  float:left;
+  width:95%; 
+  background-color: #EFF2F7;
+  height:100%;
+  overflow: auto;
+ 
+}
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Pos from '@/components/Pos'
+import HelloWorld from '@/components/HelloWorld'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Pos',
+      component: Pos
+    },
+    {
+      path: '/in',
+      name: 'hello',
+      component: HelloWorld
+    }
+  ]
+})
